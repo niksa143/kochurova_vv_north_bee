249c1176c2ba59e1ea999a0c26afc84f52a642d5
Вероника Кочурова <veronikakocurova@MacBook-Pro-Veronika.local>
2026-06-14 15:23:07 +0300

    Сделаны правки готового проекта. Итоговая версия

--- north_bee/genders/models.py
+++ north_bee/genders/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Gender(models.Model):
     gender_name=models.CharField(max_length=20,verbose_name="Пол")
 
+    def __str__(self):
+        return self.gender_name
--- north_bee/status/models.py
+++ north_bee/status/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Status(models.Model):
     status_name=models.CharField(max_length=20,verbose_name="Статус")
 
+    def __str__(self):
+        return self.status_name
